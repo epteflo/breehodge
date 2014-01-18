af3300ea597a8da4dde8824fd657f8bb007033bf
Bali Miklós <mike@dbx.hu>
2014-01-18 21:29:21 +0100

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Breehodge::Application.config.secret_key_base = 'f525389b7794f5429081d8c423ba66ce5287f4c9e18b25cc533b236c04fd1f4973b24226feb9ca4ffab940786c8e111bbd1660bfa7a1847f3271e5c2201148c0'
